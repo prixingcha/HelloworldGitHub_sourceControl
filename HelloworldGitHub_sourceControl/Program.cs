ae3d10feaccd71d6cb3c966f46549f5667bf5e37
pratik singh <pratik_cha@hotmail.com>
2016-04-30 17:47:29 -0500

    my Github first, added write

--- HelloworldGitHub_sourceControl/Program.cs
+++ HelloworldGitHub_sourceControl/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloworldGitHub_sourceControl
 {
@@ -10,6 +6,8 @@ namespace HelloworldGitHub_sourceControl
     {
         static void Main(string[] args)
         {
+            Console.Write("hello world Git Hub");
+
         }
     }
 }
